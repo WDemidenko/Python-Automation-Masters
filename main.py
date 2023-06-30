3d2635969987fe5a0be161cfeeb5219190a32197
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-06-30 18:51:10 +0200

    Add build url function, to get correct url for choosen type, brand and model of car

--- main.py
+++ main.py
@@ -10,7 +10,55 @@ import requests as requests
 from bot_notifications import send_notification
 
 BASE_URL = "https://auto.ria.com"
-SEARCH_URL = "search/?indexName=auto&categories.main.id=1&brand.id[0]=79&model.id[0]=2104&country.import.usa.not=0&price.currency=1&abroad.not=0&custom.not=1&damage.not=0&page=0&size=1000"
+TYPE_CAR = "Легковые"
+BRAND_CAR = "Toyota"
+MODEL_CAR = "Sequoia"
+
+
+def get_soup(search):
+    link = urljoin(BASE_URL, search)
+    page = requests.get(link).content
+    soup = BeautifulSoup(page, "html.parser")
+    return soup
+
+
+def build_url():
+    search = "search/?indexName=auto,order_auto,newauto_search"
+    soup = get_soup(search)
+    select_element = soup.find("select", class_="selected grey", id="category")
+    categories = select_element.find_all("option")
+
+    for category in categories:
+        if category.text == TYPE_CAR:
+            search += f"&categories.main.id={category['value']}"
+            break
+
+    soup = get_soup(search)
+    brands = soup.select('select[name="brand.id[0]"] option')
+    brand_id = ""
+
+    for brand in brands:
+        if brand.text.strip().startswith(BRAND_CAR):
+            brand_id = brand["value"]
+            search += f"&brand.id[0]={brand_id}"
+            break
+
+    soup = get_soup(search)
+    models = soup.select(
+        f'select.selected.grey.hide[data-brand="{brand_id}"][name="model.id[0]"] option'
+    )
+
+    for model in models:
+        if model.text.strip().startswith(MODEL_CAR):
+            model_id = model["value"]
+            search += f"&model.id[0]={model_id}"
+            break
+    # Add "Авто пригнані із США", "Участь в ДТП - Так"
+    search += "&country.import.usa.not=0&damage.not=0"
+    # Add no pagination
+    search += "page=0&size=10000"
+
+    return search
 
 
 def create_connection():
@@ -30,7 +78,8 @@ def create_tables(conn):
         id_auto INTEGER PRIMARY KEY,
         name TEXT,
         url TEXT,
-        price INTEGER
+        price INTEGER,
+        bidfax_link TEXT
     )
     """
     photos_table_sql = """
@@ -77,7 +126,10 @@ def check_ad(processed_ad: dict, ad_id, price_in_db):
     else:
         if processed_ad["price"] != price_in_db:
             # Price has changed, update the price in the database and send notification
-            cursor.execute("UPDATE ads SET price = ? WHERE id_auto = ?", (processed_ad["price"], ad_id))
+            cursor.execute(
+                "UPDATE ads SET price = ? WHERE id_auto = ?",
+                (processed_ad["price"], ad_id),
+            )
             conn.commit()
             message = f"Price has changed, old price is {price_in_db}"
             send_notification(ad_id, message)
@@ -93,13 +145,11 @@ def check_ads(processed_ads: dict):
         for ad in ads:
             ad_id, price_in_db = ad
             processed_ad = processed_ads.get(ad_id)
-
             check_ad(processed_ad, ad_id, price_in_db)
             if processed_ad:
                 processed_ads.pop(ad_id)
 
         for id_auto, new_ad in processed_ads.items():
-
             photos_links = new_ad.pop("image_links")
             insert_ad(conn, new_ad)
             insert_photos(conn, id_auto, photos_links)
@@ -107,35 +157,39 @@ def check_ads(processed_ads: dict):
 
 
 def parse_single_ad(link: str):
-    url = urljoin(BASE_URL, link)
-    page = requests.get(url).content
-    soup = BeautifulSoup(page, "html.parser")
+    soup = get_soup(link)
+
     photo_tag = soup.find("div", class_="carousel-inner").find(
         "script", type="application/ld+json"
     )
     json_images = json.loads(photo_tag.string)
-
     image_links = [image["contentUrl"] for image in json_images["image"]]
+
     info_tag = soup.find("script", id="ldJson2", type="application/ld+json")
-
     json_info = json.loads(info_tag.string)
-
     id_auto = re.findall(r"\d+", link)[0]
     name = json_info["name"]
     url = json_info["url"]
     price = int(json_info["offers"]["price"])
 
-    ad_data = {"id_auto": id_auto, "name": name, "url": url, "price": price, "image_links": image_links}
+    bidfax_tag = soup.find("script", attrs={"data-bidfax-pathname": True})
+    bidfax_link = urljoin(BASE_URL, bidfax_tag["data-bidfax-pathname"])
+
+    ad_data = {
+        "id_auto": id_auto,
+        "name": name,
+        "url": url,
+        "price": price,
+        "image_links": image_links,
+        "bidfax_link": bidfax_link,
+    }
 
     return ad_data
 
 
-def parse_ads():
+def parse_ads(url):
     while True:
-        url = urljoin(BASE_URL, SEARCH_URL)
-
-        page = requests.get(url).content
-        soup = BeautifulSoup(page, "html.parser")
+        soup = get_soup(url)
         ticket_items = soup.find_all("section", class_="ticket-item")
         processed_ads = {}
 
@@ -143,15 +197,14 @@ def parse_ads():
             div = ticket_item.find("div", class_="hide")
             link = div.get("data-link-to-view")
             ad_data = parse_single_ad(link)
-
             ad_id = int(ad_data["id_auto"])
-
             processed_ads[ad_id] = ad_data
 
         check_ads(processed_ads)
-    # Wait for 10 minutes before fetching again
+        # Wait for 10 minutes before fetching again
         time.sleep(40)
 
 
 if __name__ == "__main__":
-    parse_ads()
+    url = build_url()
+    parse_ads(url)
